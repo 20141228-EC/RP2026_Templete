02ed1ae6a50ded665baa6afda6023399dbc5ca64
TimandRyan-Avril <1504634213@qq.com>
2026-05-31 23:17:35 +0800

    [fix]修复遥控器开控拨轮值不一定为0

--- Application/ProtocolLayer/rc_protocol.c
+++ Application/ProtocolLayer/rc_protocol.c
@@ -110,6 +110,10 @@ void rc_sensor_update(rc_sensor_t *rc_sen, uint8_t *rxBuf)
 
 	rc_info->thumbwheel.value = ((int16_t)rxBuf[16] | ((int16_t)rxBuf[17] << 8)) & 0x07ff;
 	rc_info->thumbwheel.value -= 1024;
+	if(abs(rc_info->thumbwheel.value)>660)
+	{
+		rc_info->thumbwheel.value=0;
+	}
 
 	rc_info->s1 = ((rxBuf[5] >> 4) & 0x000C) >> 2;
 	rc_info->s2 = (rxBuf[5] >> 4) & 0x0003;	
