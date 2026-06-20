702923ac89c468ffe47960dfd2d4d727aa43a8a3
13392511380 <3206849113@qq.com>
2026-06-20 12:16:22 +0800

    修复 usart.c 等文件的问题

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -98,7 +98,7 @@ void MX_UART7_Init(void)
 
   /* USER CODE END UART7_Init 1 */
   huart7.Instance = UART7;
-  huart7.Init.BaudRate = 2000000;  // imu_xrobot需要用2000000波特率
+  huart7.Init.BaudRate = 2000000;  //imu_xrobot
   huart7.Init.WordLength = UART_WORDLENGTH_8B;
   huart7.Init.StopBits = UART_STOPBITS_1;
   huart7.Init.Parity = UART_PARITY_NONE;
@@ -141,7 +141,7 @@ void MX_UART8_Init(void)
 
   /* USER CODE END UART8_Init 1 */
   huart8.Instance = UART8;
-  huart8.Init.BaudRate = 119200;
+  huart8.Init.BaudRate = 115200;
   huart8.Init.WordLength = UART_WORDLENGTH_8B;
   huart8.Init.StopBits = UART_STOPBITS_1;
   huart8.Init.Parity = UART_PARITY_NONE;
